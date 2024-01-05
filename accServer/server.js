d8fb11fa85ffb0a67ec287384dfacecfe3dcabc7
mati <mateuszeksli@o2.pl>
2024-01-05 03:17:46 +0100

    gierka v0.2

--- accServer/server.js
+++ accServer/server.js
@@ -5,6 +5,7 @@ const wss = new WebSocket.Server(
         console.log("ws startuje na porcie 3000")
     });
 
+// const players = [{ id: 0, active: true, x: "0.00", y: "0.00", z: "0.00", color: "aqua", size: 50, ph: 1900, pw: 1900 }]
 const players = []
 const obstacles = []
 const colors = [
@@ -58,18 +59,25 @@ wss.on('connection', (ws, req) => {
             ws.send(JSON.stringify({ status: "init", id: id, players: players, obstacles: obstacles }))
         } else if (status == "sender") {
             let decoded = JSON.parse(message)
+            players[id].id = id
             players[id].x = decoded.x
             players[id].y = decoded.y
             players[id].z = decoded.z
             players[id].color = color
         } else if (status == "reciver") {
             let decoded = JSON.parse(message)
-            console.log(decoded)
             if (decoded.status == "add") {
                 obstacles.push(decoded.obstacle)
             }
             if (decoded.status == "remove") {
-                obstacles.splice(obstacles.indexOf(decoded.obstacle), 1)
+                if (obstacles.includes(decoded.obstacle)) {
+                    obstacles.splice(obstacles.indexOf(decoded.obstacle), 1)
+                }
+            }
+            if (decoded.status == "up" && players[id] && players[id] != null) {
+                players[id].size = decoded.size
+                players[id].ph = decoded.ph
+                players[id].pw = decoded.pw
             }
             ws.send(JSON.stringify({ status: "data", players: players, obstacles: obstacles }))
         }
@@ -78,8 +86,8 @@ wss.on('connection', (ws, req) => {
     ws.on('close', (e) => {
         console.log("left")
         if (status == "sender") {
-            colors.push(color)
-            players[id] = null
+            // colors.push(color)
+            // players[id] = null
         } else if (status == "reciver") {
             if (players[id]) {
                 players[id].active = false
